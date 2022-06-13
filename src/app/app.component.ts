008f0d9325ceccda1f244bd0a204b26b8009f14d
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2022-06-13 17:16:50 -0500

    calculadora con funcipones
    suma resta multiplicaicon exponente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+  numero1:number = 0;
+  numero2:number = 0;
+
+  resultado:number = 0;
+
+  sumar():void{
+    this.resultado = this.numero1+this.numero2;
+  }
+
+  resta():void{
+    this.resultado = this.numero1-this.numero2;
+  }
+
+  mutipplicacion():void{
+    this.resultado = this.numero1*this.numero2;
+  }
+
+  division():void{
+    this.resultado = this.numero1/this.numero2;
+  }
+
+  exponente():void{
+    this.resultado = this.numero1**this.numero2;
+  }
 }
